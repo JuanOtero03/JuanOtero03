5b6a03d8811e498c50b753440d31f5782195d9b5
JUAN PABLO OTERO FERNANDEZ <oterofernandezjuan@gmail.com>
2025-02-07 21:56:27 -0500

    Add files via upload

---
+++ Clase Tienda.java
@@ -0,0 +1,31 @@
+public class Clase Tienda {
+
+	public int Nombre;
+
+	public int Direccion;
+
+	private int Telefono;
+
+	private List<Producto> Productos;
+
+	public void AgregarProducto(Productos producto) {
+
+	}
+
+	public void ListaProductos() {
+
+	}
+
+	public void EliminarProducto() {
+
+	}
+
+	public void AgregarProducto() {
+
+	}
+
+	public Clase  Producto BuscarProducto() {
+		return null;
+	}
+
+}
---
+++ Clase TipoProducto.java
@@ -0,0 +1,11 @@
+public class clase TipoProducto {
+
+	private String Nombre_producto;
+
+	private String Descripcion;
+
+	public void Mostrar_info() {
+
+	}
+
+}
